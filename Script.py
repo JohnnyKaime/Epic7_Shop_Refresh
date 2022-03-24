7939207a0fa83647ff95c6b492e86a86636a73f9
JohnnyKaime <3565155@myuwc.ac.za>
2022-03-24 23:03:21 +0200

    Fix some bugs?

--- Script.py
+++ Script.py
@@ -13,10 +13,11 @@ def checkBookmarks():
     if Mystic_pos:
         Mystic_point=pyautogui.center(Mystic_pos)
         x = Mystic_point[0]
+        y = Mystic_point[1]
         pyautogui.click(x+800,y+50)
         #Shold use size here from FindEmulatorSize
         time.sleep(0.5)
-        pyautogui.click(pyautogui.locateOnScreen('Pics\\Buy_button_Mystic.png', grayscale=False,confidence=0.5))
+        pyautogui.click(pyautogui.locateOnScreen('Pics\\Buy_button_Mystic.png', grayscale=False,confidence=0.7))
         global mystic
         mystic+=1
         time.sleep(1)
@@ -27,7 +28,7 @@ def checkBookmarks():
         y = Coven_point[1]
         pyautogui.click(x+800,y+50)
         time.sleep(0.5)
-        pyautogui.click(pyautogui.locateOnScreen('Pics\\Buy_button_Covenant.png', grayscale=False,confidence=0.5))
+        pyautogui.click(pyautogui.locateOnScreen('Pics\\Buy_button_Covenant.png', grayscale=False,confidence=0.7))
         global covenant
         covenant+=1
         time.sleep(1)
@@ -38,14 +39,13 @@ def scroll():
     time.sleep(1.5)
 
 def Refresh():
-    #while keyboard.is_pressed('q') == False: 
-    RB_pos=pyautogui.locateOnScreen('Pics\\refresh_button.png', grayscale=False,confidence=0.5)
+    RB_pos=pyautogui.locateOnScreen('Pics\\refresh_button.png', grayscale=False,confidence=0.7)
     checkBookmarks()
     scroll()
     checkBookmarks()     
     pyautogui.click(RB_pos)
-    time.sleep(1.5)
-    Confirm_pos=pyautogui.locateOnScreen('Pics\\confirm button.png', grayscale=False,confidence=0.5)
+    time.sleep(2)
+    Confirm_pos=pyautogui.locateOnScreen('Pics\\confirm button.png', grayscale=False,confidence=0.7)
     pyautogui.click(Confirm_pos)
     global autos
     autos+=1
