d9a50df1c26f825461f9e611221397e8d65e1918
brunocordioli072@ <brunocordioli072@gmail.com>
2022-08-23 19:12:59 -0300

    feat: added random clicks, retries on refresh, general refactor

--- Script.py
+++ Script.py
@@ -1,66 +1,126 @@
-#Dependency Python ofc 
-#pip install pywin32
-#pip install Pillow
-#pip install keyboard
-#pip install pyautogui
-#pip install opencv-python
+from ctypes.wintypes import POINT
+import pyautogui
+import time
+import keyboard
+import math
+from random import random
 
-import win32gui, win32api, win32con, pyautogui, sys, time, keyboard
-def checkBookmarks():
-    Mystic_pos=pyautogui.locateOnScreen('Pics\\mystic.png',grayscale=False,confidence=0.7)
-    Coven_pos=pyautogui.locateOnScreen('Pics\\covenant.png',grayscale=False,confidence=0.7)
 
-    if Mystic_pos:
-        Mystic_point=pyautogui.center(Mystic_pos)
-        x = Mystic_point[0]
-        y = Mystic_point[1]
-        pyautogui.click(x+800,y+50)
-        #Shold use size here from FindEmulatorSize
+# https://stackoverflow.com/questions/23700822/weighted-random-coordinates
+def randomPoint(aroundX, aroundY, scale=1, density=2):
+    angle = random()*2*math.pi
+
+    x = random()
+    if x == 0:
+        x = 0.0000001
+
+    distance = scale * (pow(x, -1.0/density) - 1)
+    return (aroundX + distance * math.sin(angle),
+            aroundY + distance * math.cos(angle))
+
+
+def random_weighted_click(aroundX, aroundY):
+    x, y = randomPoint(aroundX, aroundY)
+    pyautogui.click(x, y)
+
+
+def random_weighted_click_pos(pos):
+    point = pyautogui.center(pos)
+    random_weighted_click(point[0], point[1])
+
+
+def locate_image_on_screen(image_path):
+    pos = pyautogui.locateOnScreen(
+        image_path, grayscale=False, confidence=0.7)
+    return pos
+
+
+def check_bookmarks():
+    mystic_pos = locate_image_on_screen("Pics\\mystic.png")
+    coven_pos = locate_image_on_screen("Pics\\covenant.png")
+
+    if mystic_pos:
+        mystic_point = pyautogui.center(mystic_pos)
+        random_weighted_click(mystic_point[0]+800, mystic_point[1]+50)
+
         time.sleep(0.5)
-        pyautogui.click(pyautogui.locateOnScreen('Pics\\Buy_button_Mystic.png', grayscale=False,confidence=0.7))
+
+        buy_button_mystic_pos = locate_image_on_screen(
+            "Pics\\Buy_button_Mystic.png")
+        random_weighted_click_pos(buy_button_mystic_pos)
+
         global mystic
-        mystic+=1
+        mystic += 1
         time.sleep(1)
-    
-    if Coven_pos:
-        Coven_point=pyautogui.center(Coven_pos)
-        x = Coven_point[0]
-        y = Coven_point[1]
-        pyautogui.click(x+800,y+50)
+
+    if coven_pos:
+        coven_point = pyautogui.center(coven_pos)
+        random_weighted_click(coven_point[0]+800, coven_point[1]+50)
+
         time.sleep(0.5)
-        pyautogui.click(pyautogui.locateOnScreen('Pics\\Buy_button_Covenant.png', grayscale=False,confidence=0.7))
+
+        buy_button_covenant_pos = locate_image_on_screen(
+            "Pics\\Buy_button_Covenant.png")
+        random_weighted_click_pos(buy_button_covenant_pos)
+
         global covenant
-        covenant+=1
+        covenant += 1
         time.sleep(1)
 
 
 def scroll():
-    pyautogui.moveTo(1263, 590)
-    pyautogui.drag(0, -350, 0.5, button="left") 
-    time.sleep(1.5)
+    x, y = randomPoint(1263, 590)
+    pyautogui.moveTo(x, y)
 
-def Refresh():
-    RB_pos=pyautogui.locateOnScreen('Pics\\refresh_button.png', grayscale=False,confidence=0.7)
-    checkBookmarks()
+    z, w = randomPoint(-350, 0.5)
+    print(z)
+    pyautogui.drag(0, z, 0.5, button="left")
+
+
+def check_store():
+    check_bookmarks()
     scroll()
-    checkBookmarks()     
-    pyautogui.click(RB_pos)
-    time.sleep(2)
-    Confirm_pos=pyautogui.locateOnScreen('Pics\\confirm button.png', grayscale=False,confidence=0.7)
-    pyautogui.click(Confirm_pos)
-    global autos
-    autos+=1
-    time.sleep(1.5)
+    check_bookmarks()
 
 
-mystic,autos,covenant = 0,0,0
+def refresh(error_counter=0):
+    try:
+        refresh_button_pos = locate_image_on_screen("Pics\\refresh_button.png")
+        random_weighted_click_pos(refresh_button_pos)
 
-try:
-    while keyboard.is_pressed('q') == False:
-        print("Total Covenant: ",covenant)
-        Refresh()
-except KeyboardInterrupt:
-    print('ctrl+c interrupted')
-    print("Total Covenant: ",covenant)
-    print("Total Mystic: ",mystic)
-    print("Total Refreshes: ",autos)
+        time.sleep(1)
+
+        confirm_pos = locate_image_on_screen("Pics\\confirm button.png")
+        random_weighted_click_pos(confirm_pos)
+
+        global refreshes
+        refreshes += 1
+
+        time.sleep(1)
+    except:
+        if error_counter < 3:
+            refresh(error_counter + 1)
+        else:
+            raise Exception(
+                "Will not try to refresh again, have tried 3 times")
+
+
+mystic, refreshes, covenant = 0, 0, 0
+
+
+def main():
+    try:
+        while keyboard.is_pressed('q') == False:
+            print("Total Covenant: ", covenant)
+            check_store()
+            refresh()
+
+    except KeyboardInterrupt:
+        print('ctrl+c interrupted')
+        print("Total Covenant: ", covenant)
+        print("Total Mystic: ", mystic)
+        print("Total Refreshes: ", refreshes)
+
+
+if __name__ == '__main__':
+    main()
